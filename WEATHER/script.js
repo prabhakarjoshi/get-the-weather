c9525675c519e800f2a51e4aaa566cc26a712549
sDEV-eloper <thisissurajdev@gmail.com>
2023-06-22 09:38:29 +0530

    Enter Key Added

--- WEATHER/script.js
+++ WEATHER/script.js
@@ -1,5 +1,5 @@
 "use strict";
-let searchicon=document.querySelector(".find");
+let searchIcon=document.querySelector(".find");
 let temp=document.querySelector(".tmp");
 let des=document.querySelector(".des");
 let pre=document.querySelector(".pre");
@@ -9,42 +9,39 @@ let humid=document.querySelector(".hum");
 var anim=document.querySelectorAll(".anim");
 
 
-// temp.textContent=23;
-// // console.log(t);
-// des.textContent="cloudy";
-// humid.textContent=76+"%";
-// pre.textContent=1000+"mb";
-// visi.textContent=100000+"m";    
+searchIcon.addEventListener('click', fetchData);
+document.addEventListener('keydown', function(event) {
+  if (event.key === "Enter") {
+    fetchData();
+  }
+});
 
-
-
-searchicon.addEventListener('click', function(){
-    var url=('https://api.openweathermap.org/data/2.5/weather?q='+document.querySelector(".city").value+'&appid=54d31c044dc4458558e7ac8fefb76646');
-    document.querySelector(".city").value="";
-    fetch(url)
+function fetchData() {
+  var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + document.querySelector(".city").value + '&appid=54d31c044dc4458558e7ac8fefb76646';
+  document.querySelector(".city").value = "";
+  fetch(url)
     .then(response => response.json())
     .then(data => {
-    document.querySelector(".search").style.display='none';    
-    document.querySelector(".result").style.display='flex';    
+      document.querySelector(".search").style.display = 'none';
+      document.querySelector(".result").style.display = 'flex';
       var tempValue = data['main']['temp'];
       var pressureValue = data['main']['pressure'];
       var humidityValue = data['main']['humidity'];
       var visibilityValue = data['visibility']
       var descValue = data['weather'][0]['description'];
-      var t=(parseInt(tempValue)-273)*1;
-      temp.textContent=t;
-      des.textContent=descValue;
-      humid.textContent=humidityValue+"%";
-      pre.textContent=pressureValue+"mb";
-      visi.textContent=visibilityValue+"m";    
+      var t = (parseInt(tempValue) - 273) * 1;
+      temp.textContent = t;
+      des.textContent = descValue;
+      humid.textContent = humidityValue + "%";
+      pre.textContent = pressureValue + "mb";
+      visi.textContent = visibilityValue + "m";
 
       for (let i = 0; i < anim.length; i++) {
-          anim[i].classList.add("animate");
-          
+        anim[i].classList.add("animate");
       }
-})
-.catch(err => alert(err));
-});
+    })
+    .catch(err => alert(err));
+}
 
 
 back.addEventListener('click', function(){
